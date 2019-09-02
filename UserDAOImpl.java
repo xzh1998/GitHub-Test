a78f13653d330e8583683075624ebb29931797f8
global01 <global01@banglc.com>
2019-09-02 15:21:16 +0800

    Commit By xzh1998,New File UserDAOImpl.java

---
+++ UserDAOImpl.java
@@ -0,0 +1,12 @@
+package banglc.dao;
+
+import java.io.Serializable;
+
+import org.springframework.stereotype.Repository;
+
+import banglc.pojo.User;
+
+@Repository
+public class UserDAOImpl extends BaseDAOImpl<User> implements UserDAO {
+
+}
